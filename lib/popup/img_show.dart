9261ef9dcf9d6615c95672e199f13093139138ce
tianmaxk <tianmaxk@126.com>
2018-07-31 22:38:25 +0800

    优化图片缩放效果

--- lib/popup/img_show.dart
+++ lib/popup/img_show.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:scalable_image/scalable_image.dart';
+import '../utils/scalableImage.dart';
 
 class ImageContent extends StatefulWidget {
   ImageContent({Key key, this.imageurl}) : super(key: key);
@@ -14,16 +14,6 @@ class ImageContent extends StatefulWidget {
 class _ImageContent extends State<ImageContent> {
   _ImageContent({this.imageurl});
   String imageurl = null;
-//  double imgwidth = 0.0;
-//  double imgheight = 0.0;
-//  double screenwid = 0.0;
-//  double screenhei = 0.0;
-//  double imagescale = 1.0;
-//  double lastscale = 1.0;
-//  double leftscroll = 0.0;
-//  double topscroll = 0.0;
-//  ScrollController _scrollController = new ScrollController();
-//  ScrollController _verscrollController = new ScrollController();
 
   @override
   void initState() {
@@ -32,77 +22,24 @@ class _ImageContent extends State<ImageContent> {
 
   @override
   void dispose() {
-//    _scrollController.dispose();
-//    _verscrollController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-//    screenwid = MediaQuery.of(context).size.width;
-//    screenhei = MediaQuery.of(context).size.height;
-//    print('screenwid='+screenwid.toString()+', screenhei='+screenhei.toString());
-//    if(imgwidth<=0.0){
-//      imgwidth = screenwid;
-//      imgheight = screenhei;
-//    }
     return Container(
       color: Colors.black,
       alignment: Alignment.center,
-//      child: GestureDetector(
-//        onScaleUpdate: (ScaleUpdateDetails details)=>onUpdateScaleImage(details),
-//        onScaleEnd: (ScaleEndDetails details)=>onScaleImage(details),
-        child: new ScalableImage(
-          imageProvider: new NetworkImage(imageurl),
-          dragSpeed: 4.0,
-          maxScale: 16.0,
-          wrapInAspect: true,
-          enableScaling: true,
-        ),
-//      ),
+      child: new ScalableImage(
+        imageProvider: new NetworkImage(imageurl),
+        dragSpeed: 4.0,
+        maxScale: 16.0,
+        wrapInAspect: true,
+        enableScaling: true,
+        screenSize: MediaQuery.of(context).size,
+      ),
     );
-//    return Container(
-//      color: Colors.black,
-//      child: SingleChildScrollView(
-//        controller: _verscrollController,
-//        child:SingleChildScrollView(
-//          scrollDirection: Axis.horizontal,
-//          controller: _scrollController,
-//          child: GestureDetector(
-//            onScaleUpdate: (ScaleUpdateDetails details)=>onUpdateScaleImage(details),
-//            onScaleEnd: (ScaleEndDetails details)=>onScaleImage(details),
-//            child: SizedBox(
-//              child: img,
-//              width: imgwidth,
-//              height: imgheight,
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
   }
-
-//  onUpdateScaleImage(ScaleUpdateDetails details){
-//    setState(() {
-//      imagescale = lastscale * details.scale;
-//      imgwidth = imagescale * screenwid;
-//      imgheight = imagescale * screenhei;
-//      leftscroll = (imagescale-1) * screenwid / 2;
-//      topscroll = (imagescale-1) * screenhei / 2;
-//      print(imagescale);
-////      print(_scrollController.position);
-////      print(_verscrollController.position);
-//      if(leftscroll>=0){
-//        _scrollController.animateTo(leftscroll, duration: new Duration(milliseconds: 1), curve: Curves.ease);
-//        _verscrollController.animateTo(topscroll, duration: new Duration(milliseconds: 1), curve: Curves.ease);
-//      }
-//    });
-//
-//  }
-//
-//  onScaleImage(ScaleEndDetails details){
-//    lastscale = imagescale;
-//  }
 }
 
 class BigImage {

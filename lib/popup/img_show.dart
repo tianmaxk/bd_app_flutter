5995b2ec86dbde60e713a7d78e9e4c8981b87914
taomin <taomin@whptp.com>
2018-08-01 14:20:49 +0800

    换了一个更好的图片缩放控件

--- lib/popup/img_show.dart
+++ lib/popup/img_show.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import '../utils/scalableImage.dart';
+//import '../utils/scalableImage.dart';
+import 'package:flutter_advanced_networkimage/zoomable_widget.dart';
 
 class ImageContent extends StatefulWidget {
   ImageContent({Key key, this.imageurl}) : super(key: key);
@@ -30,14 +31,19 @@ class _ImageContent extends State<ImageContent> {
     return Container(
       color: Colors.black,
       alignment: Alignment.center,
-      child: new ScalableImage(
-        imageProvider: new NetworkImage(imageurl),
-        dragSpeed: 4.0,
-        maxScale: 16.0,
-        wrapInAspect: true,
-        enableScaling: true,
-        screenSize: MediaQuery.of(context).size,
-      ),
+        child: ZoomableWidget(
+          maxScale: 16.0,
+          minScale: 0.5,
+          child: Image.network(imageurl),
+        ),
+//      child: new ScalableImage(
+//        imageProvider: new NetworkImage(imageurl),
+//        dragSpeed: 4.0,
+//        maxScale: 16.0,
+//        wrapInAspect: true,
+//        enableScaling: true,
+//        screenSize: MediaQuery.of(context).size,
+//      ),
     );
   }
 }
